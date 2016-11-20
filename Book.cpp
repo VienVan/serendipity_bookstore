5abf92e0e9ba44e76d30608b5904e9a2c36500be
syedsufyan1232 <stihami11@gmail.com>
2016-11-20 03:07:07 -0800

    Update Book.cpp
    Added an function for overloading the >> operator. Slightly modified the << function.

--- Book.cpp
+++ Book.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 
-Book::Book(){}
+Book::Book(){
+	author = "";       //Set all of these to default values. Make sure that they do not mess up sorting operations, etc.
+	title = "";
+	ISBN = "";
+	publisher = "";
+	date = "";
+	quantity = "";
+	wholesale = "";
+	retail = "";
+}
 Book::Book(string a, string t, string i, string p, int d, int m, int y, int q, double w, double r)
 {
 	author = a;
 	title = t;
-	isbn = i;
+	ISBN = i;
 	publisher = p;
 	date = Date(m, d, y);
 	quantity = q;
@@ -18,8 +27,15 @@ Book::Book(string a, string t, string i, string p, int d, int m, int y, int q, d
 	retail = r;
 }
 
-ostream& operator<<(ostream& out, const Book&thi)
+ostream& operator<<(ostream& out, const Book &thi)
 {
-	out << thi.author << " " << thi.title << " " << thi.isbn << " " << thi.publisher << " " << thi.date << " " << thi.quantity << " " << thi.wholesale << " " << thi.retail;
+	out << thi.author << ' ' << thi.title << ' ' << thi.ISBN << ' ' << thi.publisher << ' ' << thi.date << ' ' << thi.quantity << ' ' << thi.wholesale << ' ' << thi.retail << endl;
 	return out;
+}
+
+istream& operator<<(istream& in, const Book&thi)
+{
+	//This requires that the read position start at the beginning of a book
+	in >> thi.author >> thi.title >> thi.ISBN >> thi.publisher >> thi.date >> thi.quantity >> thi.wholesale >> thi.retail;
+	return in;
 }
