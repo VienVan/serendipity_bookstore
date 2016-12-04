9b73b65e6382b189808b477321bb8c9637ad090e
syedsufyan1232 <stihami11@gmail.com>
2016-12-03 23:06:15 -0800

    Update main.cpp
    Added important progress notes to the end of main file

--- main.cpp
+++ main.cpp
@@ -15,3 +15,17 @@ int main()
     runner = nullptr;
 	return 0;
 }
+
+/*Problems and modifications:
+ -"Fixed" linker errors by filling in with empty functions
+ -Massive revamping of all input/output stream functions/operators
+ -Changed runner to a pointer to Module for sake of including polymorphism
+ -New data (and especially Date) entry formatting
+ -Fix in.ignore()
+ -Add exceptions and templates; polymorphism done
+ -Still eed to complete all three branch modules
+ -Documentation and Deliverables
+ For Ronald: I changed the cout/cin functions for both the Book and Date classes. Here's how it needs to be done:
+ Tabs delimit anything that has spaces (Title, Author, Publisher). Everything else has a space at the end.
+ For Date, make sure entry is in format: "MM/DD/YYYY"
+*/
