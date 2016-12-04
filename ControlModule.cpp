66603451135d1f0455777758133ece4684b0541d
syedsufyan1232 <stihami11@gmail.com>
2016-12-03 23:11:40 -0800

    Update ControlModule.cpp

--- ControlModule.cpp
+++ ControlModule.cpp
@@ -26,7 +26,7 @@ void ControlModule::userInteraction()
 {
     int choice;
     do{
-        cout << "\t Serendipity Booksellers\n\t\t Main Menu" << endl
+        cout << "\n\t Serendipity Booksellers\n\t\t Main Menu" << endl
         << "1. Cashier Module" << endl
         << "2. Inventory Database Module" << endl
         << "3. Report Module" << endl
