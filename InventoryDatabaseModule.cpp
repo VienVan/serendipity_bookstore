9e29eeefd754d26f1636d24daa09799be140b172
EPKgit <ericpaulkim@gmail.com>
2016-12-07 16:14:12 -0800

    Add files via upload

--- InventoryDatabaseModule.cpp
+++ InventoryDatabaseModule.cpp
@@ -65,13 +65,11 @@ void InventoryDatabaseModule::userInteraction()
 			{
 				cout << "Enter the title of the book you would like to edit: ";
 				getline(cin, temp);
-				cout << temp;
 				Book * build = (*inv)[(inv->searchTitle(temp))];
-				cout << *build;
 				string choice2;
 				do
 				{
-					cout << "Edit Book Menu" << endl
+					cout << endl << "Edit Book Menu" << endl
 						<< "1. Edit Title" << endl
 						<< "2. Edit Author" << endl
 						<< "3. Edit ISBN" << endl
@@ -79,59 +77,53 @@ void InventoryDatabaseModule::userInteraction()
 						<< "5. Edit Date" << endl
 						<< "6. Edit Quantity" << endl
 						<< "7. Edit Wholesale Value" << endl
-						<< "8. Return to Inventory Database Module";
+						<< "8. Return to Inventory Database Module" << endl
+						<< "Your current book's data is:" << endl << *build
+						<< "Enter Your Choice: ";
 					getline(cin, choice2);
-					
 					switch (atoi(choice2.c_str()))
 					{
 						case 1:
 						{
-							cout << "Your current book's data is:" << endl << *build;
 							cout << "Enter a new title: ";
 							getline(cin, temp);
 							build->setTitle(temp);
 						}break;
 						case 2:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new author: ";
-								  getline(cin, temp);
-								  build->setAuthor(temp);
+							cout << "Enter a new author: ";
+							getline(cin, temp);
+							build->setAuthor(temp);
 						}break;
 						case 3:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new ISBN: ";
-								  getline(cin, temp);
-								  build->setISBN(temp);
+							cout << "Enter a new ISBN: ";
+							getline(cin, temp);
+							build->setISBN(temp);
 						}break;
 						case 4:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new publisher: ";
-								  getline(cin, temp);
-								  build->setPublisher(temp);
+							cout << "Enter a new publisher: ";
+							getline(cin, temp);
+							build->setPublisher(temp);
 						}break;
 						case 5:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new date in the format MM/DD/YYYY e.g. 04/05/1998: ";
-								  getline(cin, temp);
-								  build->setDate(Date(temp));
+							cout << "Enter a new date in the format MM/DD/YYYY e.g. 04/05/1998: ";
+							getline(cin, temp);
+							build->setDate(Date(temp));
 						}break;
 						case 6:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new quantity: ";
-								  getline(cin, temp);
-								  build->setQuantity(atoi(temp.c_str()));
+							cout << "Enter a new quantity: ";
+							getline(cin, temp);
+							build->setQuantity(atoi(temp.c_str()));
 						}break;
 						case 7:
 						{
-								  cout << "Your current book's data is:" << endl << *build;
-								  cout << "Enter a new author: ";
-								  getline(cin, temp);
-								  build->setWholesale(atoi(temp.c_str()));
+							cout << "Enter a new author: ";
+							getline(cin, temp);
+							build->setWholesale(atoi(temp.c_str()));
 						}break;
 						default:{ cout << endl << "INPUT ERROR" << endl; }
 					}
