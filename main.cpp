197ac9b810f8378a37087c1086bb63d6ab2d34e3
syedsufyan1232 <stihami11@gmail.com>
2016-12-07 20:12:49 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 
 int main()
 {
+    //Name Block:
+    cout << "CIS 22B\nFinal Project\nTeam #1\n\nMembers:\nEric Kim\nRonald Martin\nSyed Tihami\nVien Van\n\n";
+    
     string filepath;
     cout << "Please enter the path to the inventory file:\n";
     getline(cin, filepath);
